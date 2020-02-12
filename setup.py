9e5b6be496ec305c8f938edd3e6681dddab3e917
Michael Hansen <michael.hansen.24@us.af.mil>
2020-02-12 17:07:52 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+"""Setup for rhasspyserver_hermes"""
+import os
+
+import setuptools
+
+this_dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_dir, "README.md"), "r") as readme_file:
+    long_description = readme_file.read()
+
+with open(os.path.join(this_dir, "requirements.txt"), "r") as requirements_file:
+    requirements = requirements_file.read().splitlines()
+
+with open(os.path.join(this_dir, "VERSION"), "r") as version_file:
+    version = version_file.read().strip()
+
+setuptools.setup(
+    name="rhasspy-server-hermes",
+    version=version,
+    author="Michael Hansen",
+    author_email="hansen.mike@gmail.com",
+    url="https://github.com/rhasspy/rhasspy-server-hermes",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6",
+)
